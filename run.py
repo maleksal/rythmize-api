0f58ee306e790ad7922d54e26265f5eb6391b159
maleksal <malek.salem.14@gmail.com>
2020-10-22 23:10:39 +0100

    add app run

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+from config import config
+from rythmize import create_app, db
+from dotenv import find_dotenv, load_dotenv
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+else:
+    raise Exception('No env settings found.')
+
+env_object = os.getenv('CONFIG_ENV')
+if  env_object not in config.keys():
+    raise Exception("CONFIG_ENV can be either Production or Development).")
+
+app = create_app(config[env_object])
+
